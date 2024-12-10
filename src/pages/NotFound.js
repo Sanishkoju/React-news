5e5c7dc7e78700704fe36188f3d567e11d7ae2a9
Sanish <sanishkoju949@gmail.com>
2024-12-10 20:22:51 +0545

    jpt

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,25 +6,24 @@ const NotFound = () => {
   return (
     <>
     <Header />
-    <section class="page_404">
-	<div class="container">
-		<div class="row">	
-		<div class="col-sm-12 ">
-		<div class="col-sm-10 col-sm-offset-1  text-center">
-		<div class="four_zero_four_bg">
-			<h1 class="text-center ">404</h1>
+    <section className="page_404">
+	<div className="container">
+		<div className="row">	
+		<div className="col-sm-12 ">
+		<div className="col-sm-10 col-sm-offset-1  text-center">
+		<div className="four_zero_four_bg">
+			<h1 className="text-center " style={{textAlign:"center"}}>404</h1>
 		
 		
 		</div>
 		
-		<div class="contant_box_404">
-		<h3 class="h2">
+		<div className="contant_box_404">
+		<h3 className="h2" style={{textAlign:"center"}}>
 		Look like you're lost
 		</h3>
 		
-		<p>the page you are looking for not avaible!</p>
-		
-		<a href="" class="link_404">Go to Home</a>
+		<p style={{textAlign:"center"}}>the page you are looking for not avaible!</p>
+
 	</div>
 		</div>
 		</div>
